303dce4d3c79976806439973bd0986693cf38fde
breadbread1984 <breadbread1984@163.com>
2021-12-09 16:57:59 +0800

    update

---
+++ README.md
@@ -0,0 +1,21 @@
+# this repository implement pretrain code for ResNet18 and ResNet34 which are missing from tf.keras.applications.
+
+## prepare dataset
+
+download ImageNet and unzip it.
+
+## train
+
+train with the following command
+
+```shell
+python3 train.py --batch_size=<batch size> --imagenet_path=<path/to/imagenet> --model=(resnet18|resnet34)
+```
+
+## save model
+
+save model from checkpoint with the following command, the saved model will be located under directory models.
+
+```shell
+python3 train.py --model=(resnet18|resnet34) --save_model
+```
--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+from os import mkdir;
 from os.path import join, exists;
 from absl import flags, app;
 import tensorflow as tf;
@@ -16,17 +17,25 @@ flags.DEFINE_float('momentum', default = 0.9, help = 'momentum');
 flags.DEFINE_enum('model', default = 'resnet18', enum_values = ['resnet18', 'resnet34'], help = 'which model to train');
 flags.DEFINE_string('imagenet_path', default = None, help = 'path to raw imagenet directory');
 flags.DEFINE_string('checkpoint', default = 'checkpoints', help = 'path to checkpoint');
+flags.DEFINE_boolean('save_model', default = False, help = 'whether to save model from checkpoint');
 
 def main(unused_argv):
 
   if exists(join(FLAGS.checkpoint, 'ckpt')):
     model = tf.keras.models.load_model(join(FLAGS.checkpoint, 'ckpt'), custom_objects = {'tf': tf}, compile = True);
     optimizer = model.optimizer;
+    if FLAGS.save_model:
+      if not exists('models'): mkdir('models');
+      model.get_layer(FLAGS.model).save(join('models', '%s.h5' % FLAGS.model));
+      exit();
   else:
     model = ImageNetRN18() if FLAGS.model == 'resnet18' else ImageNetRN34();
     optimizer = tf.keras.optimizers.SGD(
       tf.keras.optimizers.schedules.ExponentialDecay(FLAGS.lr, decay_steps = FLAGS.decay_epochs * 1281167 / FLAGS.batch_size, decay_rate = FLAGS.decay_rate), 
       momemtum = FLAGS.momentum);
+    model.compile(optimizer = optimizer,
+                  loss = [tf.keras.losses.SparseCategoricalCrossentropy(name = 'ce_loss')],
+                  metrics = [tf.keras.metrics.SparseCategoricalAccuracy(name = 'acc')]);
   # create dataset
   imagenet = ImageNet(FLAGS.imagenet_path);
   options = tf.data.Options();
